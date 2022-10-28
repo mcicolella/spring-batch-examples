7864e09f5a472435c370d26e86afd72ca9027783
Mauro Cicolella <mcicolella@libero.it>
2022-10-28 15:26:05 +0200

    Update spring-batch-base-example

--- spring-batch-base-example/src/main/java/net/emmecilab/springbatch/job/DemoJob.java
+++ spring-batch-base-example/src/main/java/net/emmecilab/springbatch/job/DemoJob.java
@@ -17,35 +17,27 @@ public class DemoJob {
 
 	@Autowired
 	private JobBuilderFactory jbf;
-	
+
 	@Autowired
 	private StepBuilderFactory sbf;
 
 	@Bean
 	public Job DemoJob() {
-
-		return jbf.get("demo job")
-				.start(firstStep())
-				.build();
+		return jbf.get("demo job").start(firstStep()).build();
 	}
 
+	@Bean
 	private Step firstStep() {
-		
-		return sbf.get("first step")
-		.tasklet(firstTask())
-		.build();
-		
+		return sbf.get("first step").tasklet(firstTask()).build();
 	}
-	
+
 	private Tasklet firstTask() {
 		return new Tasklet() {
-
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				System.out.println("This is our first Spring Batch task");
 				return RepeatStatus.FINISHED;
 			}
-			
 		};
 	}
 }
